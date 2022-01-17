510a42dfe242bd1d59d57fec3f7efa7863fda080
Vektor-Ko <VektorKollective@gmail.com>
2022-01-16 16:50:25 -0800

    Simple console application for reading a JSON using Path and then retrieving value.

---
+++ JSON-Value-Retriever/JSONReader.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace JSON_Value_Retriever
+{
+    internal class JSONReader
+    {
+        private string _JSONString;
+        private JObject _JSONObject;
+
+        public JSONReader()
+        {
+
+        }
+
+        #region Private Methods
+        private void ParseJSON(string json)
+        {
+            try
+            {
+                _JSONObject = JObject.Parse(json);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Creates and initializes a JSONReader object from a json string
+        /// </summary>
+        static public JSONReader ParseJson(string json)
+        {
+            JSONReader reader = new JSONReader();
+            try
+            {
+                reader.ParseJSON(json);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return reader;
+        }
+
+        public string GetValue(string key)
+        {
+            if (_JSONObject == null)
+            {
+                return "Empty JSON";
+            }
+            else
+            {
+                try
+                {
+                    var value = _JSONObject.GetValue(key);
+                    return value?.ToString() ?? "Not Found...";
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ JSON-Value-Retriever/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Text;
+
+namespace JSON_Value_Retriever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                    return;
+                else
+                {
+                    string path = args[0];
+
+                    using (StreamReader r = new StreamReader(path))
+                    {
+                        string json = r.ReadToEnd();
+                        JSONReader reader = JSONReader.ParseJson(json);
+                        if (reader != null)
+                        {
+                            Console.WriteLine("Enter value looking for: ");
+                            var input = Console.ReadLine();
+
+                            string value = reader.GetValue(input);
+                            Console.WriteLine(value);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
