511907a1481f0b4e86efb35e1ba9e4b136e4f191
Vektor-Ko <VektorKollective@gmail.com>
2022-01-16 17:50:38 -0800

    Json reader test project

---
+++ JSONReaderTests/JSONReaderTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JSON_Value_Retriever;
+using System.IO;
+
+namespace JSONReaderTests
+{
+    [TestClass]
+    public class JSONReaderTests
+    {
+
+        [TestMethod]
+        public void JSONReaderFromPath()
+        {
+            string[] paths = { @"C:\Users", "dev", "JSONS", "DummyJSON.json" };
+            string fullPath = Path.Combine(paths);
+            JSONReader reader = JSONReader.ParseJsonPath(fullPath);
+            Assert.IsNotNull(reader);
+
+            string value = reader.GetValue("Dummy");
+            Assert.AreEqual("JSON", value);
+
+        }
+    }
+}
